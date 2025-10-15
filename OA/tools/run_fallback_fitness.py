360be11b01770034570db03790e85b02d0827004
Rayavarapu Surya Harshith <suryaharshithr@gmail.com>
2025-10-15 09:44:45 +0530

    Delete OA directory

--- OA/tools/run_fallback_fitness.py
+++
@@ -1,4 +0,0 @@
-from app.llm import fallback_plan
-import json
-plan = fallback_plan('get fit in 30 days', 30)
-print(json.dumps(plan, indent=2))
--- OA/tools/run_fallback_test.py
+++
@@ -1,5 +0,0 @@
-from app.llm import fallback_plan
-import json
-
-plan = fallback_plan('prepare for exams of subjects like maths, physics, chemistry', 7)
-print(json.dumps(plan, indent=2))
